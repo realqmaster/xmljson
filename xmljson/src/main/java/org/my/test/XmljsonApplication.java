65294fcb79ed82a7e4f4e81426d39914dc8dbf4c
Maurizio Serrazanetti <m.serrazanetti@crif.com>
2020-05-13 16:35:21 +0200

    Disabled default SpringBoot MVC security filters

--- xmljson/src/main/java/org/my/test/XmljsonApplication.java
+++ xmljson/src/main/java/org/my/test/XmljsonApplication.java
@@ -1,13 +1,30 @@
 package org.my.test;
 
+import com.crif.cff.shared.commons.util.CFFTypesRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication	//(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = {
+	    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+	})
 public class XmljsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmljsonApplication.class, args);
+	}
+	
+	@Configuration
+	public class XmljsonConfig {
+		
+		@Bean
+		public CFFTypesRepository cffTypesRepository() {
+			return new CFFTypesRepository();
+		}
 	}
 
 }
